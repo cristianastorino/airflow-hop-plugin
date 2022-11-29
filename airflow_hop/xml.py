cef2b9df2349ee14e989cea7b91b52ad1000df66
Cristian Astorino <cristian.astorino@agilelab.it>
2022-11-29 14:58:08 +0100

    fix project_home variable

--- airflow_hop/xml.py
+++ airflow_hop/xml.py
@@ -178,7 +178,8 @@ class XMLBuilder:
 
         project_home = Element('variable')
         project_home.append(self.__generate_element('name','PROJECT_HOME'))
-        project_home.append(self.__generate_element('value',self.project_home))
+        project_home.append(self.__generate_element('value',self.project_folder))
+        root.append(project_home)
 
         jdk_debug = Element('variable')
         jdk_debug.append(self.__generate_element('name','jdk.debug'))
